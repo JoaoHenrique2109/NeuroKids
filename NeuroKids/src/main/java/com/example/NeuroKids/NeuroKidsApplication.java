f0d3d5cbcc300bc9d49f49cbd6ab11c00290e75b
João Silva <jhenriqueb2109@gmail.com>
2026-04-27 23:18:21 -0300

    Add files via upload

---
+++ NeuroKids/src/main/java/com/example/NeuroKids/NeuroKidsApplication.java
@@ -0,0 +1,13 @@
+package com.example.NeuroKids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuroKidsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeuroKidsApplication.class, args);
+	}
+
+}
---
+++ NeuroKids/src/test/java/com/example/NeuroKids/NeuroKidsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NeuroKids;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuroKidsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
